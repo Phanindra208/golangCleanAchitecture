05df0cbd10c8e84eecfbe4305d015eaba4228c54
phanindra baddula <phanindra208@hotmail.com>
2020-05-09 14:28:28 +0800

    first commit

---
+++ hello/go.mod
@@ -0,0 +1,10 @@
+module example.com/user/hello
+
+go 1.14
+
+require (
+	github.com/go-kit/kit v0.8.0
+	github.com/gorilla/mux v1.7.4
+	github.com/prometheus/client_golang v0.9.3
+	github.com/spf13/viper v1.6.3
+)
---
+++ hello/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"runtime/trace"
+
+	"example.com/user/hello/domain"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigFile(`config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool(`debug`) {
+		log.Println("Service RUN on DEBUG mode")
+	}
+}
+func main() {
+	f, err := os.Create("trace.out")
+	if err != nil {
+		log.Fatalf("failed to create trace output file: %v", err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalf("failed to close trace file: %v", err)
+		}
+	}()
+
+	if err := trace.Start(f); err != nil {
+		log.Fatalf("failed to start trace: %v", err)
+	}
+	defer trace.Stop()
+
+	var port = viper.GetString("server.address")
+
+	var (
+		httpAddr = flag.String("http", port, "http listen address")
+	)
+	flag.Parse()
+	ctx := context.Background()
+	// our domain service
+	srv := domain.NewService()
+	errChan := make(chan error)
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+
+	// mapping endpoints
+	endpoints := domain.Endpoints{
+		GetEndpoint:      domain.MakeGetEndpoint(srv),
+		StatusEndpoint:   domain.MakeStatusEndpoint(srv),
+		ValidateEndpoint: domain.MakeValidateEndpoint(srv),
+	}
+
+	// HTTP transport
+	go func() {
+		log.Println("domain is listening on port:", *httpAddr)
+		handler := domain.NewHTTPServer(ctx, endpoints)
+		errChan <- http.ListenAndServe(*httpAddr, handler)
+	}()
+
+	log.Fatalln(<-errChan)
+}
